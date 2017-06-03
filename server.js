f25f425d72ea4004dc78369d0e11535067399c28
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-06-03 09:25:38 +0200

    serving index statically

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.use(express.static('public'))
+
+/*app.get('/', function (req, res) {
   res.send('Hello World!')
-});
+});*/
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
