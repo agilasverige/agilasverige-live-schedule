3cb2d0ff521859c87e25d472836917229b178345
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-06-03 13:04:34 +0200

    Simple endpoint that parses the schedule

--- server.js
+++ server.js
@@ -3,23 +3,47 @@ const app = express();
 
 app.use(express.static('public'))
 
-/*app.get('/', function (req, res) {
-  res.send('Hello World!')
-});*/
 
-const fs = require('fs');
-const csv = require('csv');
+app.get('/api/schedule', function (req, res) {
+  function toObject(row) {
+    function parseTime(s) {
+      const parts = s.split(':');
+      const now = new Date();
+      now.setHours(parts[0], parts[1]);
+      return now;
+    }
 
-var input = fs.createReadStream('data/1.csv');
-input
-  .pipe(csv.parse())
-  .pipe(csv.transform(function(record) {
-     return record.map(function(value) {
-       return value;
-     });
-  }))
-  .pipe(csv.stringify())
-  .pipe(process.stdout);
+    function parseDuration(s) {
+      const parts = s.trim().split('-');
+      if (parts.length < 2) return null;
+      return {'start': parseTime(parts[0]), 'stop': parseTime(parts[1])};
+    }
+    var obj = parseDuration(row[0]);
+    if (!obj) return null;
+    obj.space = {'title': row[1], 'speaker': row[2]};
+    obj.tab =   {'title': row[3], 'speaker': row[4]};
+    return obj;
+  }
+  const fs = require('fs');
+  const csv = require('csv');
+  const JSONStream = require('JSONStream');
+
+  var input = fs.createReadStream('data/2017.csv');
+  input
+    .pipe(csv.parse())
+    .pipe(csv.transform(function(record) {
+      //console.log(record);
+      //return record.map(function(value) {
+      //  return value;
+      //});
+      return toObject(record);
+    }))
+    .pipe(JSONStream.stringify())
+    //.pipe(csv.stringify())
+    //.pipe(process.stdout);)
+    .pipe(res);
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
