ebe18dd2ace2dd48d18ba518caec1ec5f6bd7253
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-06-03 10:34:11 +0200

    csv parser spike

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ app.use(express.static('public'))
   res.send('Hello World!')
 });*/
 
+const fs = require('fs');
+const csv = require('csv');
+
+var input = fs.createReadStream('data/1.csv');
+input
+  .pipe(csv.parse())
+  .pipe(csv.transform(function(record) {
+     return record.map(function(value) {
+       return value;
+     });
+  }))
+  .pipe(csv.stringify())
+  .pipe(process.stdout);
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
