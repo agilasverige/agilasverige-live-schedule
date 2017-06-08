364f45e290860de92ce5c7860169e926bf5533e7
vidstige <samuel.carlsson@volumental.com>
2017-06-08 16:11:01 +0200

    fix local != UTC snafu

--- test/agilaSverige.spec.js
+++ test/agilaSverige.spec.js
@@ -15,19 +15,23 @@ var streamToString = function(stream, callback) {
 describe('cvs2json', function() {
   it('test1', function(done) {
     // Override Date
-    const original = Date;
+    const originalDate = Date;
     Date = function() {
-      return new original(original.parse("2017-06-06T12:34:56.000Z"));
+      return new originalDate(originalDate.parse("2017-06-06T12:34:56.000Z"));
     };
-    Date.parse = original.parse;
+    Date.parse = originalDate.parse;
 
     const input = MemoryStream.createReadStream("Tid,Space,Talare,Tab,Talare\n08:00-09:00,a,b,c,d");
     const output = new MemoryStream();
     streamToString(output, function(str) {
-      console.log(str);
       const actual = JSON.parse(str);
       const expected = [
-        {"start":"2017-06-06T06:00:00.000Z","stop":"2017-06-06T07:00:00.000Z","space":{"title":"a","speaker":"b"},"tab":{"title":"c","speaker":"d"}}
+        {
+          "start": new originalDate("2017-06-06 08:00:00").toISOString(),
+          "stop":  new originalDate("2017-06-06 09:00:00").toISOString(),
+          "space": {"title": "a", "speaker": "b"},
+          "tab":   {"title": "c", "speaker": "d"}
+        }
       ];
       assert.deepEqual(actual, expected);
       done();
