9fab2e440e3b256eb4d119c6ded01527b023a040
vidstige <samuel.carlsson@volumental.com>
2017-06-08 11:18:19 +0200

    refactor: mock `Date`
    Without this the unit test would only work today.

--- test/agilaSverige.spec.js
+++ test/agilaSverige.spec.js
@@ -14,13 +14,20 @@ var streamToString = function(stream, callback) {
 
 describe('cvs2json', function() {
   it('test1', function(done) {
+    // Override Date
+    const original = Date;
+    Date = function() {
+      return new original(original.parse("2017-06-06T12:34:56.000Z"));
+    };
+    Date.parse = original.parse;
+
     const input = MemoryStream.createReadStream("Tid,Space,Talare,Tab,Talare\n08:00-09:00,a,b,c,d");
     const output = new MemoryStream();
     streamToString(output, function(str) {
       console.log(str);
       const actual = JSON.parse(str);
       const expected = [
-        {"start":"2017-06-08T06:00:00.000Z","stop":"2017-06-08T07:00:00.000Z","space":{"title":"a","speaker":"b"},"tab":{"title":"c","speaker":"d"}}
+        {"start":"2017-06-06T06:00:00.000Z","stop":"2017-06-06T07:00:00.000Z","space":{"title":"a","speaker":"b"},"tab":{"title":"c","speaker":"d"}}
       ];
       assert.deepEqual(actual, expected);
       done();
