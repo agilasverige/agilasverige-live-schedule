67b4f68c1cda471c0e9e2f99024d7b09df7402c4
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-06-11 17:31:57 +0200

    refactor: mock dom & xhr

--- test/schedule.spec.js
+++ test/schedule.spec.js
@@ -1,6 +1,17 @@
 const schedule = require('../src/schedule');
+const jsdom = require("jsdom");
+const FakeXMLHttpRequest = require('fakexmlhttprequest');
 
 describe('index', function() {
   it('test1', function() {
+    // mock window
+    const dom = new jsdom.jsdom(`<!DOCTYPE html><p>Hello world</p>`);
+    global.window = dom;
+
+    // Mock XMLHttpRequest 
+    const xhr = FakeXMLHttpRequest;
+    global.XMLHttpRequest = xhr;
+
+    schedule();
   });
 });
