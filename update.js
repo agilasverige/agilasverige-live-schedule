578b64d1e6e648f4918e6a4a05bac7771d03f383
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2017-06-18 22:47:11 +0200

    Schedule update script now defaults to generating schedule starting today. Can be overridden by command-line argument

--- update.js
+++ update.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 const agilaSverige = require('./agilaSverige');
 
-const startDay = new Date("2017-06-06");
+function getStartDate() {
+  if (process.argv.length < 3) {
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 0, 0, 0);
+  }
+  return new Date(process.argv[2]);
+}
+
+
+const startDay = getStartDate();
+console.log("Generating json schedule starting on " + startDay.toDateString())
 var input = fs.createReadStream('data/2017/program.csv');
 var output = fs.createWriteStream('public/data/program.json');
 agilaSverige.cvs2json(input, output, {startDay});
