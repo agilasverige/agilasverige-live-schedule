c06a7f2768087c3974084084e094dbddecf24f8f
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-06-18 09:19:46 +0200

    frontend unit test

--- test/schedule.spec.js
+++ test/schedule.spec.js
@@ -1,17 +1,35 @@
 const schedule = require('../src/schedule');
+const chai = require('chai');
+const sinon = require('sinon');
 const jsdom = require("jsdom");
-const FakeXMLHttpRequest = require('fakexmlhttprequest');
+
+chai.should();
 
 describe('index', function() {
-  it('test1', function() {
-    // mock window
-    const dom = new jsdom.jsdom(`<!DOCTYPE html><p>Hello world</p>`);
-    global.window = dom;
+    beforeEach(function() {
+        this.xhr = sinon.useFakeXMLHttpRequest();
 
-    // Mock XMLHttpRequest 
-    const xhr = FakeXMLHttpRequest;
-    global.XMLHttpRequest = xhr;
+        this.requests = [];
+        this.xhr.onCreate = function(xhr) {
+            this.requests.push(xhr);
+        }.bind(this);
 
-    schedule();
+        global.XMLHttpRequest = this.xhr;
+    });
+
+    afterEach(function() {
+        this.xhr.restore();
+    });
+
+    it('test1', function() {
+      // mock window
+      const dom = new jsdom.jsdom(`<!DOCTYPE html><input type="range" id="now-slider"><span id="now-display"></span>`);
+      global.window = dom;
+      global.document = dom;
+
+      schedule();
+      const data = [{start: "123", stop: "456"}];
+      this.requests[0].respond(200, {'Content-Type': 'text/json'}, JSON.stringify(data));
   });
+    
 });
